1f16ab2c3e371364298f51786081a48b4c9a4011
carlosalexbauer-eng <carlosalexbauer@gmail.com>
2026-04-16 15:04:33 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,12 @@
+import { BotaoDataHoje } from "@/components/BotaoDataHoje";
 import { Dashboard } from "@/components/dashboard/Dashboard";
 
 const Index = () => {
   return (
-    <div>
-      <div>Antes do dashboard</div>
+    <>
       <Dashboard />
-      <div>Depois do dashboard</div>
-    </div>
+      <BotaoDataHoje />
+    </>
   );
 };
 

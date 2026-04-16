3977a85612482d517bd2ea21c3ad465a52e10b79
carlosalexbauer-eng <carlosalexbauer@gmail.com>
2026-04-16 11:45:21 -0300

    Update App.tsx
    adiciona tela administrativa

--- src/App.tsx
+++ src/App.tsx
@@ -30,3 +30,11 @@ const App = () => (
 );
 
 export default App;
+import "./App.css";
+import AdminDashboard from "./pages/AdminDashboard";
+
+function App() {
+  return <AdminDashboard />;
+}
+
+export default App;
